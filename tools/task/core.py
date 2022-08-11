64ae14754b9259aa9d27e3140790ceb6a3d8526f
Adnan <alamin493641@gmail.com>
2022-08-11 19:56:15 +0600

    rewrite

--- tools/task/core.py
+++ tools/task/core.py
@@ -29,12 +29,12 @@ def keyword_process(keyword=None):
         # print('ssss',sen)
         newdatalist =[]
         for send in sen:
-            datal=len(sen)
-            if 'CD' in send or 'CC' in send or 'DT' in send or 'EX' in send or 'FW' in send or 'IN' in send or 'LS' in send or 'MD' in send or 'NN' in send or 'NNP' in send or 'NNPS' in send or 'PDT' in send or 'POS' in send or 'PRP' in send or 'PRP$' in send or 'RP' in send or 'TO' in send or 'UH' in send or 'VBG' in send or 'VBD' in send or 'VBN' in send or 'VBZ' in send or 'WDT' in send or '.' in send or 'WRB' in send or 'VB' in send:
+            datal=len(sen)  
+            if 'CD' in send or 'CC' in send or 'DT' in send or 'EX' in send or 'FW' in send or 'IN' in send or 'LS' in send or 'MD' in send or 'NNP' in send or 'NNPS' in send or 'PDT' in send or 'POS' in send or 'PRP' in send or 'PRP$' in send or 'RP' in send or 'TO' in send or 'UH' in send or 'VBG' in send or 'VBD' in send or 'VBN' in send or 'VBZ' in send or 'WDT' in send or '.' in send or 'WRB' in send:
                 data=send[0]
                 newdatalist.append(data)
 
-            if 'NNS' in send or 'VBP' in send or 'JJ' in send or 'JJR' in send or 'RB' in send or 'RBR' in send or 'RBS' in send or 'JJS' in send:
+            if 'NNS' in send or 'NN' in send or 'VB' in send or 'VBP' in send or 'JJ' in send or 'JJR' in send or 'RB' in send or 'RBR' in send or 'RBS' in send or 'JJS' in send:
                 list=[]
                 data=send[0]
                 print('send ',data)
@@ -75,11 +75,11 @@ def content_process(content=None):
     
     for send in sen:
         datal=len(sen)
-        if 'CD' in send or 'CC' in send or 'DT' in send or 'EX' in send or 'FW' in send or 'IN' in send or 'LS' in send or 'MD' in send or 'NN' in send or 'NNP' in send or 'NNPS' in send or 'PDT' in send or 'POS' in send or 'PRP' in send or 'PRP$' in send or 'RP' in send or 'TO' in send or 'UH' in send or 'VBG' in send or 'VBD' in send or 'VBN' in send or 'VBP' in send or  'VBZ' in send or 'WDT' in send or '.' in send or 'WRB' in send or 'VB' in send:
+        if 'CD' in send or 'CC' in send or 'EX' in send or 'DT' in send  or 'FW' in send or 'IN' in send or 'LS' in send or 'MD' in send or 'NNP' in send or 'NNPS' in send or 'PDT' in send or 'POS' in send or 'PRP' in send or 'PRP$' in send or 'RP' in send or 'TO' in send or 'UH' in send or 'VBG' in send or 'VBD' in send or 'VBN' in send or 'VBP' in send or  'VBZ' in send or 'WDT' in send or '.' in send or 'WRB' in send:
             data=send[0]
             newdatalist.append(data)
 
-        if 'NNS' in send or 'JJ' in send or 'JJR' in send or 'RB' in send or 'RBR' in send or 'RBS' in send or 'JJS' in send:
+        if 'NNS' in send or 'NN' in send or 'VB' in send or 'JJ' in send or 'JJR' in send or 'RB' in send or 'RBR' in send or 'RBS' in send or 'JJS' in send:
             list=[]
             data=send[0]
             # print('data',data)
@@ -89,7 +89,7 @@ def content_process(content=None):
                     list.append(word)           
 
             if list:
-                print('list',list)
+                # print('list',list)
                 data=random.choice(list)
                 newdatalist.append(data)
                     
